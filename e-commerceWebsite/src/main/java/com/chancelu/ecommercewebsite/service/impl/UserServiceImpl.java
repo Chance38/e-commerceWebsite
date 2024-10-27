77ed313b28fc7d78a8326c1ab4d2722a257a5361
Chance38 <chance89308@gmail.com>
2024-10-27 23:50:35 +0800

    帳號功能 - 檢查註冊過的 email - 隱藏返回的密碼

--- e-commerceWebsite/src/main/java/com/chancelu/ecommercewebsite/service/impl/UserServiceImpl.java
+++ e-commerceWebsite/src/main/java/com/chancelu/ecommercewebsite/service/impl/UserServiceImpl.java
@@ -4,12 +4,18 @@ import com.chancelu.ecommercewebsite.dao.UserDao;
 import com.chancelu.ecommercewebsite.dto.UserRegisterRequest;
 import com.chancelu.ecommercewebsite.model.User;
 import com.chancelu.ecommercewebsite.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 @Component
 public class UserServiceImpl implements UserService {
+
+    private final static Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Autowired
     private UserDao userDao;
@@ -21,6 +27,17 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Integer register(UserRegisterRequest userRegisterRequest) {
+
+        //檢查註冊的email
+        User user = userDao.getUserByEmail(userRegisterRequest.getEmail());
+
+        //如果email已存在，回傳400給前端，告知email已使用過
+        if (user != null) {
+            log.warn("該 email {} 已經被註冊", userRegisterRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        //創建帳號
         return userDao.createUser(userRegisterRequest);
     }
 }
